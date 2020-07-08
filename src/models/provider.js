69ac15b2db331c7a6e3d7c8aaee7d7617f9c5913
Abhishek Sharma <abhi243461@hotmail.com>
2020-07-08 19:36:40 +0530

    feat: Add provider model

---
+++ src/models/provider.js
@@ -0,0 +1,108 @@
+const mongoose = require('mongoose')
+
+const providerSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    commercialName: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value))
+                throw new Error('Provide a valid email!')
+        }
+    },
+    mobile: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isMobilePhone(value, ['am-Am', 'ar-AE', 'ar-BH', 'ar-DZ', 'ar-EG', 'ar-IQ', 'ar-JO', 'ar-KW', 'ar-SA', 'ar-SY', 'ar-TN', 'be-BY', 'bg-BG', 'bn-BD', 'cs-CZ', 'da-DK', 'de-DE', 'de-AT', 'de-CH', 'el-GR', 'en-AU', 'en-CA', 'en-GB', 'en-GG', 'en-GH', 'en-HK', 'en-MO', 'en-IE', 'en-IN', 'en-KE', 'en-MT', 'en-MU', 'en-NG', 'en-NZ', 'en-PK', 'en-RW', 'en-SG', 'en-SL', 'en-UG', 'en-US', 'en-TZ', 'en-ZA', 'en-ZM', 'en-ZW', 'es-CL', 'es-CO', 'es-CR', 'es-EC', 'es-ES', 'es-MX', 'es-PA', 'es-PY', 'es-UY', 'et-EE', 'fa-IR', 'fi-FI', 'fj-FJ', 'fo-FO', 'fr-BE', 'fr-FR', 'fr-GF', 'fr-GP', 'fr-MQ', 'fr-RE', 'he-IL', 'hu-HU', 'id-ID', 'it-IT', 'ja-JP', 'kk-KZ', 'kl-GL', 'ko-KR', 'lt-LT', 'ms-MY', 'nb-NO', 'ne-NP', 'nl-BE', 'nl-NL', 'nn-NO', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sl-SI', 'sk-SK', 'sr-RS', 'sv-SE', 'th-TH', 'tr-TR', 'uk-UA', 'vi-VN', 'zh-CN', 'zh-HK', 'zh-MO', 'zh-TW'], { strictMode: false }))
+                throw new Error('Provide a valid phone number')
+        }
+    },
+    addresses: [{
+        street: { type: String, trim: true, required: true },
+        city: { type: String, trim: true, required: true },
+        state: { type: String, trim: true, required: true },
+        country: { type: String, trim: true, required: true },
+        zip: { type: String, trim: true, required: true },
+    }],
+    banksAccounts: [{
+        bankName: { type: String, requied: true, trim: true },
+        bankAddress: {
+            street: { type: String, requied: true, trim: true },
+            city: { type: String, required: true, trim: true },
+            state: { type: String, required: true, trim: true },
+            country: { type: String, required: true, trim: true },
+            zip: { type: String, required: true, trim: true },
+        }, requied: true,
+        account_number: { type: String, required: true, index: true, trim: true },
+    }],
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password'))
+                throw new Error('Provide a strong password!')
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            require: true
+        }
+    }]
+}, {
+    timestamps: true
+})
+
+providerSchema.methods.toJSON = function () {
+    const providerObj = this.toObject()
+    delete providerObj.password
+    return providerObj
+}
+
+providerSchema.methods.generateAuthToken = async function () {
+    const token = jwt.sign({ _id: this._id.toString() }, 'myToken')
+    this.tokens = this.tokens.concat({ token })
+    await this.save()
+
+    return token
+}
+
+providerSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) throw new Error('No user found with this email!')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) throw new Error('Incorrect password!')
+
+    return user
+}
+
+providerSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const Provider = mongoose.model('Provider', providerSchema)
+
+module.exports = Provider
